09d01a5ac203b5ade49e696bc0786cc1c0575d36
Devanshu Shah <56106207+Devanshu24@users.noreply.github.com>
2021-08-07 16:18:30 +0530

    Seperate Linting workflow; Setup pre-commit (#9)
    * Seperate Linting workflow; Setup pre-commit
    
    * Install pytest in the pytest CI
    
    * Update codecov.yml
    
    * Update codecov
    
    * Add some files so that coverage can be empty
    
    * Update codecov.yml
    
    * Revamp the structure!
    
    * Tweak codecov workflow
    
    * Precommit everything!
    
    * Update pre-commit
    
    * Fix errors, remove erroneous flake8 checks
    
    * Trigger codecov
    
    * Restructure

---
+++ code_soup/ch5/dummy.py
@@ -0,0 +1,6 @@
+def add_nums(a, b):
+    return a + b
+
+
+def sub_nums(a, b):
+    return a - b
---
+++ tests/test_ch5/test_dummy.py
@@ -0,0 +1,6 @@
+from code_soup.ch5.dummy import add_nums
+
+
+def test_add_nums():
+    """Tests addition of numbers"""
+    assert add_nums(2, 4) == 6
