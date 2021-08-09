a78d0405e7d9a3ef1fdcaf968f43dedab9879fbd
Somesh Kumar Singh <f20180175@goa.bits-pilani.ac.in>
2021-08-09 16:26:57 +0530

    Removed Unnecessary Arg Descriptions

--- code_soup/ch5/datasets/mnist.py
+++ code_soup/ch5/datasets/mnist.py
@@ -8,17 +8,8 @@ class MnistDataset(Dataset):
     """
     `MNIST <http://yann.lecun.com/exdb/mnist/>`_ Dataset. Built using TorchVision Dataset class.
     Args:
-        root (string): Root directory of dataset where ``MNIST/processed/training.pt``
-            and  ``MNIST/processed/test.pt`` exist.
-        train (bool, optional): If True, creates dataset from ``training.pt``,
-            otherwise from ``test.pt``.
-        download (bool, optional): If true, downloads the dataset from the internet and
-            puts it in root directory. If dataset is already downloaded, it is not
-            downloaded again.
         transform (callable, optional): A function/transform that  takes in an PIL image
             and returns a transformed version. E.g, ``transforms.RandomCrop``
-        target_transform (callable, optional): A function/transform that takes in the
-            target and transforms it.
     """
 
     def __init__(self, transform: callable = None):
