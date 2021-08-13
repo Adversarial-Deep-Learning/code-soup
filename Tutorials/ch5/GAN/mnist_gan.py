7b7d0c44d79a9b7d52f7d1a98308b11cc0dedf0c
Shreyas-Bhat <shreyasbhat2001@gmail.com>
2021-08-13 09:43:15 +0530

    adding allconvnet

--- Tutorials/ch5/GAN/mnist_gan.py
+++ Tutorials/ch5/GAN/mnist_gan.py
@@ -49,10 +49,7 @@ if __name__ == "__main__":
 
     # Loading the dataset
     transform = transforms.Compose(
-        [
-            transforms.ToTensor(),
-            transforms.Normalize((0.5,), (0.5,)),
-        ]
+        [transforms.ToTensor(), transforms.Normalize((0.5,), (0.5,))]
     )
     dataset = MnistDataset(transform=transform)
     dataloader = torch.utils.data.DataLoader(
@@ -61,11 +58,7 @@ if __name__ == "__main__":
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
     gan = GAN(
-        image_size=28,
-        channels=1,
-        latent_dims=128,
-        device=torch.device("cpu"),
-        lr=0.02,
+        image_size=28, channels=1, latent_dims=128, device=torch.device("cpu"), lr=0.02
     )
 
     for epoch in range(epochs):
