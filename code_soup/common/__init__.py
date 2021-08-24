1abb489cdc250af52800a2101f6251755829a549
someshsingh22 <someshsingh9414@gmail.com>
2021-08-24 17:58:53 +0530

    Initial Tests

---
+++ code_soup/common/__init__.py
@@ -0,0 +1,1 @@
+from code_soup.common.vision.perturbations import VisualPerturbation
---
+++ code_soup/common/perturbations.py
@@ -0,0 +1,12 @@
+from abc import ABC, abstractmethod
+
+
+class Perturbation(ABC):
+    """
+    Docstring for Abstract Class Perturbation
+    """
+
+    @abstractmethod
+    @classmethod
+    def __init__(self):
+        pass
