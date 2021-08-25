685f31b05203558fdd7e973639ebb84605aca76f
devaletanmay <devaletanmay@gmail.com>
2021-08-25 17:22:39 +0530

    Visual Perturbation temp

--- code_soup/common/vision/perturbations.py
+++ code_soup/common/vision/perturbations.py
@@ -3,13 +3,19 @@ from typing import Union
 
 import numpy as np
 import torch
+import torch.nn as nn
+
+from math import log10
 
 from code_soup.common.perturbation import Perturbation
 
 
 class VisualPerturbation(Perturbation):
     """
-    Docstring for VisualPerturbations
+    An abstract method for various Visual Perturbation Metrics
+        Methods
+        __init__(self, original : Union[np.ndarray, torch.Tensor], perturbed: Union[np.ndarray, torch.Tensor])
+            - init method
     """
 
     def __init__(
@@ -21,15 +27,42 @@ class VisualPerturbation(Perturbation):
         Docstring
         #Automatically cast to Tensor using the torch.from_numpy() in the __init__ using if
         """
-        raise NotImplementedError
 
-    def calculate_LPNorm(self, p: Union[int, str]):
-        raise NotImplementedError
+        if type(original) == torch.Tensor:
+            self.original = original
+        else:
+            self.original = torch.from_numpy(original)
+        print(self.original.shape)
 
-    def calculate_PSNR(self):
-        raise NotImplementedError
+        if type(perturbed) == torch.Tensor:
+            self.perturbed = perturbed
+        else:
+            self.perturbed = torch.from_numpy(perturbed)
 
-    def calculate_RMSE(self):
+    def flatten(self, array : torch.tensor) -> torch.Tensor:
+        return array.flatten()
+
+    def totensor(self, array : np.ndarray) -> torch.Tensor:
+        return torch.from_numpy(array)
+
+    def subtract(self,original : torch.Tensor, perturbed : torch.Tensor) -> torch.Tensor:
+        return torch.sub(original, perturbed)
+
+    def calculate_LPNorm(self, p: Union[int, str]) -> float:
+        if p == 'inf':
+            return torch.linalg.vector_norm(self.flatten(self.subtract(self.original,self.perturbed)), ord = float('inf')).item()
+        elif p == 'fro':
+            return self.calculate_LPNorm(2)
+        else:
+            return torch.linalg.norm(self.flatten(self.subtract(self.original,self.perturbed)), ord = p).item()
+
+    def calculate_PSNR(self) -> float:
+        return 20 * log10(255.0/self.calculate_RMSE())
+
+    def calculate_RMSE(self) -> float:
+        # return torch.sqrt(torch.mean(self.subtract(self.flatten(self.original), self.flatten(self.perturbed))**2)).item()
+        # loss = nn.MSELoss()
+        # return (loss(self.original, self.perturbed)**0.5).item()
         raise NotImplementedError
 
     def calculate_SAM(self):
