deb9b17b0e037bf4cd272f1f5013b23a7d30f352
soham-chitnis10 <sohamchitnis10@gmail.com>
2021-08-30 23:03:50 +0530

    Restructured according to PEP8

--- code_soup/common/utils/checkpoints.py
+++ code_soup/common/utils/checkpoints.py
@@ -2,11 +2,46 @@ import torch
 
 
 class Checkpoints:
-    @classmethod
-    def save(self, PATH, model):
-        torch.save(model.state_dict(), PATH)
+    """
+    A class to save and load checkpoints
+    """
 
     @classmethod
-    def load(self, PATH, model):
-        model.load_state_dict(torch.load(PATH))
-        return model
+    def save(self, PATH, model, EPOCH, optimizer=None, LOSS=None):
+        """
+        Parameters
+        ----------
+        PATH : str
+            - The path where the model is saved
+        model:
+            - The model which is saved
+        EPOCH: int
+            - Checkpoint
+        optimizer (optional):
+            - Default: None , optimizer saved at the checkpoint
+        LOSS (optional):
+            - Default: None , loss saved at the checkpoint
+
+        Saves the model state and checkpoint state with optimizer and loss if specified
+        """
+        torch.save(
+            {
+                "epoch": EPOCH,
+                "model_state_dict": model.state_dict(),
+                "optimizer_state_dict": optimizer.state_dict(),
+                "loss": LOSS,
+            },
+            PATH,
+        )
+
+    @classmethod
+    def load(self, PATH):
+        """
+        Paramters
+        ---------
+        PATH: str
+            - The path from where the model is loaded
+
+        Returns the loaded model
+        """
+        return torch.load(PATH)
