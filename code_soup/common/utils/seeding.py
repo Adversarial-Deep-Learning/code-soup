c814329af4d306be4fb493f500e835a98fe88985
soham-chitnis10 <sohamchitnis10@gmail.com>
2021-08-30 16:54:00 +0530

    Updated seed function

--- code_soup/common/utils/seeding.py
+++ code_soup/common/utils/seeding.py
@@ -13,3 +13,5 @@ class Seeding:
         torch.manual_seed(self.seed)
         torch.cuda.manual_seed(self.seed)
         random.seed(self.seed)
+        torch.backends.cudnn.deterministic = True
+        torch.backends.cudnn.benchmark = False
