c73e0aa4605dc5648497fcf18ae3f8d1da43398c
Gunjan Chhablani <chhablani.gunjan@gmail.com>
2021-08-30 16:19:34 +0530

    Add ZOO Attack (#42)

---
+++ code_soup/common/utils.py
@@ -0,0 +1,16 @@
+import random
+
+import numpy as np
+import torch
+
+
+def torch_seed(value=42):
+    """Set random seed for everything.
+    Args:
+        value (int): Seed
+    """
+    np.random.seed(value)
+    torch.manual_seed(value)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+    random.seed(value)
---
+++ tests/test_common/test_vision/test_models/test_simple_cnn_classifier.py
@@ -0,0 +1,17 @@
+import unittest
+
+import torch
+import torch.nn as nn
+
+from code_soup.common.vision.models.simple_cnn_classifier import SimpleCnnClassifier
+
+
+class TestSimpleCnnClassifier(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls) -> None:
+        cls.model = SimpleCnnClassifier(input_shape=(3, 32, 32), num_labels=10)
+
+    def test_step(self):
+        model_input = torch.randn(5, 3, 32, 32)
+        model_output = self.model(model_input)
+        self.assertEqual(model_output.shape, torch.Size([5, 10]))
