960c33402bdf0bf0ef9c3638f21484e7420ea7bd
soham-chitnis10 <sohamchitnis10@gmail.com>
2021-09-01 18:22:23 +0530

    Updated test_checkpoints.py

--- tests/test_common/test_utils/test_checkpoints.py
+++ tests/test_common/test_utils/test_checkpoints.py
@@ -12,12 +12,12 @@ from code_soup.common.utils import Checkpoints
 
 class TheModelClass(nn.Module):
     """
-    Model class for test_save function
+    Model class for tests
     """
 
-    def __init__(self, input_length: int):
+    def __init__(self):
         super(TheModelClass, self).__init__()
-        self.dense = nn.Linear(int(input_length), 1)
+        self.dense = nn.Linear(2, 1)
         self.activation = nn.Sigmoid()
 
     def forward(self, x):
@@ -29,7 +29,7 @@ class TestCheckpoints(unittest.TestCase):
         """
         Test that the model is saved
         """
-        model_save = TheModelClass(7)
+        model_save = TheModelClass()
         optimizer = optim.SGD(model_save.parameters(), lr=0.01, momentum=0.9)
         loss = 0.5
         epoch = 10
@@ -40,23 +40,22 @@ class TestCheckpoints(unittest.TestCase):
             epoch,
             loss,
         )
-        self.assert_(os.path.isfile("./input/test_model.pth"))
+        self.assertTrue(os.path.isfile("./input/test_model.pth"))
 
     def test_load(self):
         """
         Test that the model is loaded
         """
-        model_load = models.resnet18(pretrained=True)
-        optimizer = optim.SGD(model_load.parameters(), lr=0.01, momentum=0.9)
+        model = TheModelClass()
+        optimizer = optim.SGD(model.parameters(), lr=0.01, momentum=0.9)
         loss = 0.5
         epoch = 10
         Checkpoints.save(
             "./input/test_model.pth",
-            model_load,
+            model,
             optimizer,
             epoch,
             loss,
         )
-        model = models.resnet18()
-        model = Checkpoints.load("./input/test_model.pth")
+        model_load = Checkpoints.load("./input/test_model.pth")
         self.assertEqual(list(model.state_dict()), list(model_load.state_dict()))
