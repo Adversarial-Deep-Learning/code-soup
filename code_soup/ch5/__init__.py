eec666b6cd76bad9c7133a185bb85021b4a390f0
Mehul Rastogi <mehul.rastogi.mehul@gmail.com>
2021-09-06 22:27:45 +0530

    Minor Refactor (#83)
    * Adding One Pixel Attack Core
    
    * Update test_one_pixel_attack.py
    
    * solve obj reference err
    
    * solve pytest err
    
    * solve merge
    
    * solve merge
    
    * minor refactor
    
    * solve circular dep issue

--- code_soup/ch5/__init__.py
+++ code_soup/ch5/__init__.py
@@ -1,1 +1,3 @@
-from code_soup.ch5.models import GAN
+from code_soup.ch5.algorithms.gan import GAN, Discriminator, Generator
+from code_soup.ch5.algorithms.one_pixel_attack import OnePixelAttack
+from code_soup.ch5.algorithms.zoo_attack import ZooAttack
--- tests/test_ch5/test_models/__init__.py
+++
@@ -1,5 +0,0 @@
-from tests.test_ch5.test_models.test_gan import (
-    TestDiscriminator,
-    TestGAN,
-    TestGenerator,
-)
