86c0116416d0c8048f542b771c8d572653e0e435
Gunjan Chhablani <chhablani.gunjan@gmail.com>
2021-09-17 09:42:15 +0530

    Add imagenet ATN classes

--- code_soup/ch5/algorithms/atn.py
+++ code_soup/ch5/algorithms/atn.py
@@ -9,6 +9,19 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
+from torch.nn.modules.activation import ReLU
+from torch.nn.modules.module import T
+
+
+class BilinearUpsample(nn.Module):
+    def __init__(self, scale_factor):
+        super(BilinearUpsample, self).__init__()
+        self.scale_factor = scale_factor
+
+    def forward(self, x):
+        return F.interpolate(
+            x, scale_factor=self.scale_factor, mode="bilinear", align_corners=True
+        )
 
 
 class ATNBase(nn.Module):
@@ -47,7 +60,21 @@ class ATNBase(nn.Module):
         return self.beta * loss_fn(x, x_hat) + loss_fn(y, y_hat)
 
 
-class AAE(ATNBase):
+class AAEBase(ATNBase):
+    def forward(self, x):
+        adv_out = self.atn(x)
+        logits = self.classifier_model(adv_out)
+        return logits
+
+
+class PATNBase(ATNBase):
+    def forward(self, x):
+        adv_out = self.atn(x)
+        logits = self.classifier_model(adv_out + x)
+        return logits
+
+
+class SimpleAAE(AAEBase):
     def __init__(
         self,
         classifier_model: torch.nn.Module,
@@ -61,7 +88,7 @@ class AAE(ATNBase):
         typ="a",
     ):
         assert typ in ["a", "b", "c"]
-        super(AAE, self).__init__(classifier_model, target_idx, alpha, beta)
+        super(SimpleAAE, self).__init__(classifier_model, target_idx, alpha, beta)
 
         self.input_shape = input_shape
 
@@ -128,13 +155,8 @@ class AAE(ATNBase):
 
         self.atn = nn.ModuleList(layers)
 
-    def forward(self, x):
-        adv_out = self.atn(x)
-        logits = self.classifier_model(adv_out)
-        return logits
 
-
-class PATN(ATNBase):
+class SimplePATN(PATNBase):
     def __init__(
         self,
         classifier_model: torch.nn.Module,
@@ -145,24 +167,178 @@ class PATN(ATNBase):
         num_channels: list = [64, 64],
         num_classes: int = 784,
     ):
-        super(PATN, self).__init__(classifier_model, target_idx, alpha, beta)
+        super(SimplePATN, self).__init__(classifier_model, target_idx, alpha, beta)
 
         layers = []
         sizes = [input_shape[0]] + num_channels
         for i in range(len(sizes) - 1):
             layers.append(nn.Conv2d(sizes[i], sizes[i + 1], kernel_size=3, padding=1))
             layers.append(nn.ReLU())
-            # TODO: Check if Max Pooling is needed here (most probably is).
+            # TODO: Check if Max Pooling is needed here.
 
         layers.append(nn.Flatten())
         layers.append(
             nn.Linear(sizes[-1] * input_shape[2] * input_shape[3], num_classes)
         )
-        layers.append(nn.Tanh())
+        layers.append(
+            nn.Tanh()
+        )  # TODO: Check if this is the right activation function for PATN
 
         self.atn = nn.ModuleList(layers)
 
-    def forward(self, x):
-        adv_out = self.atn(x)
-        logits = self.classifier_model(adv_out + x)
-        return logits
+
+class BaseDeconvAAE(AAEBase):
+    def __init__(
+        self,
+        classifier_model: torch.nn.Module,
+        pretrained_backbone: torch.nn.Module,
+        target_idx: int,
+        alpha: float = 1.5,
+        beta: float = 0.010,
+        backbone_output_shape: list = [192, 35, 35],
+    ):
+
+        if backbone_output_shape != [192, 35, 35]:
+            raise ValueError("Backbone output shape must be [192, 35, 35].")
+
+        super(BaseDeconvAAE, self).__init__(classifier_model, target_idx, alpha, beta)
+
+        layers = [
+            pretrained_backbone,
+            nn.ZeroPad2d((1, 1, 1, 1)),
+            nn.ConvTranspose2d(192, 512, kernel_size=4, stride=2, padding=1),
+            nn.ReLU(),
+            nn.ConvTranspose2d(512, 256, kernel_size=3, stride=2, padding=1),
+            nn.ReLU(),
+            nn.ConvTranspose2d(256, 128, kernel_size=4, stride=2, padding=1),
+            nn.ReLU(),
+            nn.ZeroPad2d((3, 2, 3, 2)),
+            nn.ConvTranspose2d(128, 3, kernel_size=3, stride=1, padding=1),
+            nn.Tanh(),
+        ]
+
+        self.atn = nn.ModuleList(layers)
+
+
+class ResizeConvAAE(AAEBase):
+    def __init__(
+        self,
+        classifier_model: torch.nn.Module,
+        target_idx: int,
+        alpha: float = 1.5,
+        beta: float = 0.010,
+    ):
+
+        super(ResizeConvAAE, self).__init__(classifier_model, target_idx, alpha, beta)
+
+        layers = [
+            nn.Conv2d(3, 128, 5, padding=2),
+            nn.ReLU(),
+            BilinearUpsample(scale_factor=0.5),
+            nn.Conv2d(128, 256, 3, padding=1),
+            nn.ReLU(),
+            BilinearUpsample(scale_factor=0.5),
+            nn.Conv2d(256, 512, 3, padding=1),
+            nn.ReLU(),
+            BilinearUpsample(scale_factor=0.5),
+            nn.Conv2d(512, 512, 1, padding=0),
+            nn.ReLU(),
+            BilinearUpsample(scale_factor=2),
+            nn.Conv2d(512, 256, 3, padding=1),
+            nn.ReLU(),
+            BilinearUpsample(scale_factor=2),
+            nn.Conv2d(256, 128, 3, padding=1),
+            nn.ReLU(),
+            BilinearUpsample(scale_factor=2),
+            nn.ZeroPad2d((3, 2, 3, 2)),
+            nn.Conv2d(128, 3, 3, padding=1),
+            nn.Tanh(),
+        ]
+
+        self.atn = nn.ModuleList(layers)
+
+
+class ConvDeconvAAE(AAEBase):
+    def __init__(
+        self,
+        classifier_model: torch.nn.Module,
+        target_idx: int,
+        alpha: float = 1.5,
+        beta: float = 0.010,
+    ):
+
+        super(ConvDeconvAAE, self).__init__(classifier_model, target_idx, alpha, beta)
+
+        layers = [
+            nn.Conv2d(3, 256, 3, stride=2, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(512, 768, 3, stride=2, padding=1),
+            nn.ReLU(),
+            nn.ConvTranspose2d(768, 512, kernel_size=3, stride=2, padding=1),
+            nn.ReLU(),
+            nn.ConvTranspose2d(512, 256, kernel_size=3, stride=2, padding=1),
+            nn.ReLU(),
+            nn.ConvTranspose2d(256, 128, kernel_size=3, stride=2, padding=1),
+            nn.ReLU(),
+            nn.ConvTranspose2d(128, 3, kernel_size=3, stride=1, padding=0),
+            nn.Tanh(),
+        ]
+
+        self.atn = nn.ModuleList(layers)
+
+
+class BaseDeconvPATN(PATNBase):
+    def __init__(
+        self,
+        classifier_model: torch.nn.Module,
+        pretrained_backbone: torch.nn.Module,
+        target_idx: int,
+        alpha: float = 1.5,
+        beta: float = 0.010,
+        backbone_output_shape: list = [192, 35, 35],
+    ):
+
+        if backbone_output_shape != [192, 35, 35]:
+            raise ValueError("Backbone output shape must be [192, 35, 35].")
+
+        super(BaseDeconvPATN, self).__init__(classifier_model, target_idx, alpha, beta)
+
+        layers = [
+            pretrained_backbone,
+            nn.ZeroPad2d((1, 1, 1, 1)),
+            nn.ConvTranspose2d(192, 512, kernel_size=4, stride=2, padding=1),
+            nn.ReLU(),
+            nn.ConvTranspose2d(512, 256, kernel_size=3, stride=2, padding=1),
+            nn.ReLU(),
+            nn.ConvTranspose2d(256, 128, kernel_size=4, stride=2, padding=1),
+            nn.ReLU(),
+            nn.ZeroPad2d((3, 2, 3, 2)),
+            nn.ConvTranspose2d(128, 3, kernel_size=3, stride=1, padding=1),
+            nn.Tanh(),  # TODO: CHeck if right activation
+        ]
+
+        self.atn = nn.ModuleList(layers)
+
+
+class ConvFCPATN(PATNBase):
+    def __init__(
+        self,
+        classifier_model: torch.nn.Module,
+        target_idx: int,
+        alpha: float = 1.5,
+        beta: float = 0.010,
+    ):
+
+        super(BaseDeconvAAE, self).__init__(classifier_model, target_idx, alpha, beta)
+
+        layers = [
+            nn.Conv2d(3, 512, 3, stride=2, padding=1),
+            nn.Conv2d(512, 256, 3, stride=2, padding=1),
+            nn.Conv2d(256, 128, 3, stride=2, padding=1),
+            nn.Flatten(),
+            nn.Linear(184832, 512),
+            nn.Linear(512, 268203),
+            nn.Tanh(),
+        ]
+
+        self.atn = nn.ModuleList(layers)
